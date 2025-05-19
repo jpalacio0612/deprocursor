ec611f1425d0bc1f9e08fa9764fec87912a8c41b
Jonathan Palacio <jpalacio0612@gmail.com>
2025-05-18 21:09:13 -0500

    add images

--- app/galeria/page.tsx
+++ app/galeria/page.tsx
@@ -6,18 +6,51 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import Image from "next/image";
+
+const IMAGES = [
+  {
+    path: "/photo-1.jpg",
+    alt: "galeria",
+    width: 1280,
+    height: 960,
+  },
+  {
+    path: "/photo-2.jpg",
+    alt: "galeria",
+    width: 1280,
+    height: 960,
+  },
+  {
+    path: "/photo-3.jpg",
+    alt: "galeria",
+    width: 960,
+    height: 1280,
+  },
+  {
+    path: "/photo-4.jpg",
+    alt: "galeria",
+    width: 1280,
+    height: 960,
+  },
+];
 
 export default function Page() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[calc(100vh-116px)] px-14">
-      <Carousel className="w-full max-w-xl h-full">
+      <Carousel className="w-full max-w-2xl h-full">
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index}>
+          {IMAGES.map((image) => (
+            <CarouselItem key={image.path}>
               <div className="p-1">
                 <Card>
-                  <CardContent className="flex aspect-square items-center justify-center p-6">
-                    <span className="text-4xl font-semibold">{index + 1}</span>
+                  <CardContent className="flex items-center justify-center">
+                    <Image
+                      src={image.path}
+                      alt={image.alt}
+                      height={image.height}
+                      width={image.width}
+                    />
                   </CardContent>
                 </Card>
               </div>
